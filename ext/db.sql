6bc65ee461c0db7b4fb773c138f60b610f2a2d5d
Cyan903 <cyanp0515@gmail.com>
2023-04-11 17:33:04 -0400

    feat: email_verified & used_storage SQL changes
    Additional changes

--- ext/db.sql
+++ ext/db.sql
@@ -2,6 +2,8 @@ create table users (
 	id int auto_increment primary key,
 	nickname varchar(32) charset utf8 not null,
 	email varchar(254) not null unique,
+	email_verified tinyint(1) default 0 not null,
+	used_storage int unsigned default 0 not null,
 	pw_bcrypt char(60) not null,
 	created_at datetime not null
 );
@@ -12,10 +14,10 @@ create table users (
 create table files (
 	id char(10) not null unique,
 	user int not null,
-	file_size int not null,
+	file_size int unsigned not null,
 	file_type char(32) not null,
 	file_pass char(60) default "",
 	file_comment char(100) default "",
-	permissions int default 0 not null,
+	permissions tinyint default 0 not null,
 	created_at datetime not null
 );
