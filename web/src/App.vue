c0c31ff4682660a8e178953b2b78c096b150c470
Cyan903 <cyanp0515@gmail.com>
2023-05-15 16:44:45 -0400

    chore: Create web

---
+++ web/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+    <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+    </nav>
+
+    <RouterView />
+</template>
+
+<style scoped>
+nav a.router-link-exact-active {
+    color: red;
+}
+</style>
