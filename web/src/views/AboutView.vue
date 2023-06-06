f22a80887cc27a9687482355197a67fc2ca95b0a
Cyan903 <cyanp0515@gmail.com>
2023-06-06 05:05:57 -0400

    feat: Simple about page

--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -1,20 +1,89 @@
 <template>
-    <div>
-        <h1>About</h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi
-            non, obcaecati laudantium quae praesentium rem, doloribus
-            temporibus, voluptates distinctio optio a necessitatibus excepturi
-            repellendus sequi. Rem cumque suscipit atque. Fugiat, iste?
-            Explicabo animi labore impedit aliquam? Voluptates magnam illo
-            impedit magni hic, pariatur earum, labore reprehenderit nobis unde
-            quibusdam voluptatem. Voluptatum quaerat suscipit eum iure? Eum,
-            amet temporibus beatae velit vel provident adipisci veniam voluptas,
-            alias odio ipsam consequatur, nulla quo. Praesentium reiciendis
-            dignissimos beatae repellendus exercitationem itaque ipsum pariatur
-            quidem ducimus! Tempore vero eaque fuga doloribus. Consectetur
-            voluptatibus vitae est velit pariatur? Quos natus fugiat vel, totam
-            et nobis.
-        </p>
+    <div class="bg-base-100 rounded-box shadow-xl p-6">
+        <h1 class="sm:text-4xl text-3xl font-bold text-center">
+            Documentation
+        </h1>
+
+        <h4 class="text-sm text-center font-bold opacity-60 my-5">
+            Frequently Asked Questions
+        </h4>
+
+        <div class="w-full md:w-2/4 m-auto">
+            <div
+                class="collapse collapse-arrow bg-base-300 mb-4"
+                @click="toggleOpen"
+            >
+                <div class="collapse-title text-xl font-medium">
+                    How do I upload and share files?
+                </div>
+                <div class="collapse-content">
+                    <p>
+                        To upload files, simply visit your
+                        <router-link to="/@me" class="link"
+                            >dashboard</router-link
+                        >. You can share a file by copying the file's ID. Please
+                        note that private files cannot be shared, and unlisted
+                        files require a password.
+                    </p>
+                </div>
+            </div>
+
+            <div
+                class="collapse collapse-arrow bg-base-300 mb-4"
+                @click="toggleOpen"
+            >
+                <div class="collapse-title text-xl font-medium">
+                    What is the difference between a private file and a unlisted
+                    file?
+                </div>
+                <div class="collapse-content">
+                    <p>
+                        A private file may only be view by you. You must be
+                        logged in to view your private file. A unlisted file may
+                        be shared but requires a password. You can change a
+                        file's permissions in your
+                        <router-link to="/@me" class="link">
+                            dashboard.
+                        </router-link>
+                    </p>
+                </div>
+            </div>
+
+            <div
+                class="collapse collapse-arrow bg-base-300 mb-4"
+                @click="toggleOpen"
+            >
+                <div class="collapse-title text-xl font-medium">
+                    How can I automate file uploading?
+                </div>
+                <div class="collapse-content">
+                    <p>Not possible at the moment.</p>
+                </div>
+            </div>
+
+            <div
+                class="collapse collapse-arrow bg-base-300 mb-4"
+                @click="toggleOpen"
+            >
+                <div class="collapse-title text-xl font-medium">
+                    Is this open source?
+                </div>
+                <div class="collapse-content">
+                    <p>
+                        Yes! You can view the source code
+                        <a :href="source" class="link">here</a>.
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+
+const source = computed(() => import.meta.env.VITE_SOURCE || "");
+
+const toggleOpen = (e: Event) =>
+    (e.target as HTMLElement).parentElement!.classList.toggle("collapse-open");
+</script>
